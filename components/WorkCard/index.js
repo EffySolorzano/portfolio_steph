8677941b9173c2ea8a7bee9be3795d4c5b7301bc
Stephanie Solórzano <stephaniesolorzano@MacBook-Pro-de-Stephanie.local>
2024-07-01 15:22:15 +0100

    updates

--- components/WorkCard/index.js
+++ components/WorkCard/index.js
@@ -1,15 +1,9 @@
-import React from "react";
+import React from 'react';
 
 const WorkCard = ({ videoSrc, name, description, onClick }) => {
   return (
-    <div
-      className="overflow-hidden rounded-lg p-2 laptop:p-4 first:ml-0 link"
-      onClick={onClick}
-    >
-      <div
-        className="relative rounded-lg overflow-hidden transition-all ease-out duration-300 h-48 mob:h-auto video-container"
-        style={{ height: "650px" }}
-      >
+    <div className="overflow-hidden rounded-lg p-2 laptop:p-4 first:ml-0 link" onClick={onClick}>
+      <div className="relative rounded-lg overflow-hidden transition-all ease-out duration-300 video-container" style={{ height: "650px" }}>
         {videoSrc ? (
           <video
             className="h-full w-full object-cover hover:scale-110 transition-all ease-out duration-300"
@@ -27,12 +21,8 @@ const WorkCard = ({ videoSrc, name, description, onClick }) => {
           </div>
         )}
       </div>
-      <h1 className="mt-5 text-3xl font-medium">
-        {name ? name : "Project Name"}
-      </h1>
-      <h2 className="text-xl opacity-50">
-        {description ? description : "Description"}
-      </h2>
+      <h1 className="mt-5 text-3xl font-medium">{name ? name : "Project Name"}</h1>
+      <h2 className="text-xl opacity-50">{description ? description : "Description"}</h2>
     </div>
   );
 };
